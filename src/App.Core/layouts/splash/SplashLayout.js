92d58823ba8b4271834daed90db1a0c1335b390a
CD2017 <cyberkidpk@users.noreply.github.com>
2022-04-25 23:21:51 +0530

    Update SplashLayout.js

--- src/App.Core/layouts/splash/SplashLayout.js
+++ src/App.Core/layouts/splash/SplashLayout.js
@@ -1,19 +1,29 @@
+import { useOktaAuth } from '@okta/okta-react';
+import { Navigate } from 'react-router-dom';
+import { Spin } from 'antd';
 import logo from '../../../App.Assets/images/Starter.Pro.Logo.png';
 
 function SplashLayout() {
+  const { oktaAuth, authState } = useOktaAuth();
+  const isOKTAEnabled = process.env.REACT_APP_isOKTAEnabled;
+
+  if (isOKTAEnabled && !authState) {
+    return <Spin />;
+  }
+  if (isOKTAEnabled && authState.isAuthenticated) {
+    return (<Navigate to="rspro2022/dash" />);
+  }
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <h2>
         REACT JS STARTER PRO 2022
       </h2>
-      <a
-        className="App-link"
-        href="rpro2022"
-        rel="noopener noreferrer"
-      >
-        Anchor
-      </a>
+      {
+      isOKTAEnabled
+        ? <Navigate to="rspro2022" className="App-link"> Home </Navigate>
+        : <Navigate to="rspro2022/dash" className="App-link"> Home </Navigate>
+      }
     </header>
   );
 }
